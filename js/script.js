352e1fc33b89737e3fa4c02a6aea2de1c7aba59d
Virtual-Zhi <lizhiqiang0071@gmail.com>
2025-08-23 19:58:28 -0400

    we

--- js/script.js
+++ js/script.js
@@ -1,28 +1,3 @@
-let prevScrollPos = window.pageYOffset;
-
-window.addEventListener('scroll', function () {
-  // current scroll position
-  const currentScrollPos = window.pageYOffset;
-
-  if (prevScrollPos >= currentScrollPos) {
-    // user has scrolled up
-    //document.querySelector('header').classList.add('show');
-    document.querySelector('.navbar').classList.add('show');
-  } else {
-    // user has scrolled down
-    //document.querySelector('header').classList.remove('show');
-    document.querySelector('.navbar').classList.remove('show');
-  }
-
-  if (currentScrollPos <= 0) {
-    document.querySelector('.navbar').classList.remove('show');
-  }
-
-  // update previous scroll position
-  prevScrollPos = currentScrollPos;
-});
-
-
 if (document.getElementById('my-work-link')) {
   document.getElementById('my-work-link').addEventListener('click', () => {
     const workSection = document.getElementById('my-work-section');
@@ -135,6 +110,31 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
+});
+
+
+let prevScrollPos = window.pageYOffset;
+
+window.addEventListener('scroll', function () {
+  // current scroll position
+  const currentScrollPos = window.pageYOffset;
+
+  if (prevScrollPos >= currentScrollPos) {
+    // user has scrolled up
+    //document.querySelector('header').classList.add('show');
+    document.querySelector('.navbar').classList.add('show');
+  } else {
+    // user has scrolled down
+    //document.querySelector('header').classList.remove('show');
+    document.querySelector('.navbar').classList.remove('show');
+  }
+
+  if (currentScrollPos <= 0) {
+    document.querySelector('.navbar').classList.remove('show');
+  }
+
+  // update previous scroll position
+  prevScrollPos = currentScrollPos;
 });
 
 
